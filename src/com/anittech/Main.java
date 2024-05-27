c01c8ae2c1bc2f9ef2a64c107c60cba42506ba6f
Anit Shrestha <anittech3@gmail.com>
2024-05-27 10:06:36 -0800

    changes

--- src/com/anittech/Main.java
+++ src/com/anittech/Main.java
@@ -3,11 +3,49 @@ package com.anittech;
 public class Main {
 
     public static void main(String[] args) {
-	// my first java program
+
+
        String name = "Anit";
        int age = 20;
        String friend = "Sudip";
+        System.out.println(name);
        System.out.println(friend);
+       System.out.print(age);
+
+
+        byte age = 20;
+        int phone = 1234567890;
+       long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = 'a';
+        boolean isAdult = true;
+
+
+
+
+        //Strings concatinate
+
+        String name1 = "Anit";
+        String name2 = "Shrestha";
+        String name3 = "Ramesh";
+        String name4 = "Laxman";
+        String name5 = "Bhawana";
+        String name6 = name1 + " "+ name2 + ", " + name3 + ", " + name4 + ", " + name5;
+        System.out.println(name6);
+
+        String dost = "Harish";
+        System.out.println(dost.charAt(0));
+
+        String name = "Ramesh";
+        System.out.println(name.length());
+
+        String rep = "Texas";
+        String rep2 = rep.replace( 'a','b');
+        System.out.println(rep2);
+
+
+
+
 
     }
 
