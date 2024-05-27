9074362f36229ce78c2d6b6840ba401fc50e4b80
Anit Shrestha <anittech3@gmail.com>
2024-05-27 09:32:50 -0800

    first java program added

---
+++ src/com/anittech/Main.java
@@ -0,0 +1,14 @@
+package com.anittech;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// my first java program
+       String name = "Anit";
+       int age = 20;
+       String friend = "Sudip";
+       System.out.println(friend);
+
+    }
+
+}
